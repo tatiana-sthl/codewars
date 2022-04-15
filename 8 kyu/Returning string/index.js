415c2aa8796fc0d8bfbaf6cc7c48c48bc7a77769
tatiana-sthl <stahli.tatiana@gmail.com>
2022-04-15 12:01:28 +0200

    add return string

---
+++ 8 kyu/Returning string/index.js
@@ -0,0 +1,12 @@
+/*
+PSEUDOCODE
+1. Write a function 
+2. Return a greeting statement that use an input
+3. The programm should return "Hello, <name> how are you doing today?"
+*/
+
+function greet(name) {
+  return "Hello, " + name + " how are you doing today?"
+}
+
+module.exports = greet
