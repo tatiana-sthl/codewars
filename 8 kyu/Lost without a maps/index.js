288512e6ba011d09354db80222db8660665a1038
tatiana-sthl <stahli.tatiana@gmail.com>
2022-04-16 15:13:15 +0200

    add lost without a map

---
+++ 8 kyu/Lost without a maps/index.js
@@ -0,0 +1,11 @@
+/*
+PSEUDOCODE
+1. Write a function who give an array of integers
+2. Return a new array with each value doubled
+*/
+
+function maps(x){
+  return x.map(n => n*2);
+}
+
+module.exports = maps
