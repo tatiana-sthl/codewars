fd4aebd96d45ae61136180e76a1a8fe9cd1c63b3
tatiana-sthl <stahli.tatiana@gmail.com>
2022-05-07 15:13:42 +0200

    product of consecutive Fib numbers 5 kyu

---
+++ 5 kyu/Product of consecutive Fib numbers/index.js
@@ -0,0 +1,27 @@
+/*
+PSEUDOCODE
+1. Write a function 
+2. Return a greeting statement that use an input
+3. The programm should return "Hello, <name> how are you doing today?"
+*/
+
+function productFib(prod){
+  var n = 0;
+  var nPlus = 1;  
+  while(n*nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
+  }
+  return [n, nPlus, n*nPlus===prod];
+}
+
+/*
+function productFib(prod){
+  let [a, b] = [0, 1];
+  while(a * b < prod) [a, b] = [b, a + b];
+  return [a, b, a * b === prod];
+}
+*/
+
+
+module.exports = productFib
