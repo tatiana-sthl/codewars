c6fdc499eb9a3f2f064e494fb7834b06955c5b18
tatiana-sthl <stahli.tatiana@gmail.com>
2022-05-27 15:50:44 +0200

    check code range 3 kyu

---
+++ 3 kyu/Check range code/index.js
@@ -0,0 +1,11 @@
+/*
+PSEUDOCODE
+1. Write a function of an array of integers a and two integers x and y
+2. Count the number of elements in the array such that `x ≤ a[i] ≤ y, where i is the 0-based index of the element.
+3. Less than 48 characters.
+*/
+
+checkRange=(a,x,y)=>a.map(v=>i+=x>v==v>y,i=0)|i
+
+
+module.exports = checkRange
